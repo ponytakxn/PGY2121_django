77e5f25b245571390e9f1cd05e7bca2a3cd5b557
ponytakxn <ju.valdesv@duocuc.cl>
2023-06-10 00:18:28 -0400

    feat: @ponytakxn migracion de productos a Django iniciada

--- bacca/models.py
+++ bacca/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
+class Categoria(models.Model):
+    id_categoria = models.AutoField(db_column="idCategoria", primary_key=True)
+    categoria = models.CharField(max_length=20, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.categoria)
+
+
+class Imagen(models.Model):
+    id_imagen = models.AutoField(db_column="idImagen", primary_key=True)
+    url_imagen = models.ImageField(upload_to="productos", null=True)
+
+
+class Producto(models.Model):
+    id_producto = models.AutoField(db_column="idProducto", primary_key=True)
+    nombre = models.CharField(max_length=40, blank=False, null=False)
+    id_categoria = models.ForeignKey(
+        "Categoria", on_delete=models.CASCADE, db_column="idCategoria"
+    )
+    imagenes = models.ManyToManyField("Imagen")
+    precio = models.IntegerField()
+    stock = models.IntegerField()
+    count = models.IntegerField()
+
+    def __str__(self):
+        return str(self.nombre)
--- bacca/views.py
+++ bacca/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Producto, Categoria, Imagen
+
 
 # Create your views here.
 def index(request):
-  context={}
-  return render(request, 'bacca/index.html', context)
+    productos = Producto.objects.all()
+    context = {"productos": productos}
+    return render(request, "bacca/index.html", context)
+
 
 def praga(request):
-  context={}
-  return render(request, 'bacca/views/praga.html', context)
+    context = {}
+    return render(request, "bacca/views/praga.html", context)
