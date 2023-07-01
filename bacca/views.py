44bfc2ae953ec45a1256fc5f23c09191c2ea817f
ponytakxn <ju.valdesv@duocuc.cl>
2023-07-01 13:02:22 -0400

    fix detalles

--- bacca/views.py
+++ bacca/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Producto, Categoria, Imagen
 
