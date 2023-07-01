ea13ce581ed2166017c58802a6a2ac055e60e9e7
ponytakxn <ju.valdesv@duocuc.cl>
2023-07-01 00:33:59 -0400

    @ponytakxn feat: autenticación de usuarios añadida

--- bacca/static/bacca/scripts/utils/ui.utils.js
+++
@@ -1,5 +0,0 @@
-export const toggleVisibility = (HTMLElement) => {
-    HTMLElement.style.visibility === 'hidden' || HTMLElement.style.cssText === ''
-        ? HTMLElement.style.visibility = 'visible'
-        : HTMLElement.style.visibility = 'hidden';
-}
--- bacca/urls.py
+++ bacca/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("productos/<str:nom>", views.productos, name="productos"),
+    path("signup/", views.signup, name="signup"),
+    path("logout/", views.signout, name="logout"),
+    path("login/", views.signin, name="login"),
 ]
